c5d236619a2934ae046ca8205edee147f903f384
pravison <pravisonjuma087@gmail.com>
2025-04-29 09:15:11 +0300

    4 commit

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Product, ProductCategory
+from .models import Product, ProductCategory, ProductName
 # Register your models here.
 
 admin.site.register(Product)
-
+admin.site.register(ProductName)
 admin.site.register(ProductCategory)
